c491014e3383c6e017c7fcac3482d0afb722cb84
Jean Parpaillon <jean.parpaillon@free.fr>
2016-01-11 10:53:07 +0100

    Fix indent

--- src/econfig_frontend.erl
+++ src/econfig_frontend.erl
@@ -10,23 +10,23 @@
 -include("econfig.hrl").
 -include("econfig_log.hrl").
 
-% External API
+												% External API
 -export([new/1,
-	 run/2]).
+		 run/2]).
 
 -type ui() :: term().
 
 -record state, {
-	  mod      :: atom(),
-	  ref      :: term(),
-	  model    :: econfig_model:t()
-	 }.
+			  mod      :: atom(),
+		  ref      :: term(),
+		  model    :: econfig_model:t()
+		 }.
 -type t() :: #state{}.
 -export_type([t/0, ui/0]).
 
 -callback start_link(Model :: econfig_model:t(), Opts :: term()) -> {ok, ui()} | {error, econfig_err()}.
 -callback run(Model :: econfig_model:t(), Config :: econfig_config:t(), ui()) -> {ok, econfig_config:t(), ui()} 
-										     | {error, term()}.
+																					 | {error, term()}.
 -callback terminate(Ref :: ui()) -> ok.
 -callback format_error(Err :: term()) -> ok.
 
@@ -34,24 +34,24 @@
 new(Model) ->
     Mod = impl(),
     case Mod:start_link(Model, []) of
-	{ok, Ref} ->
-	    #state{mod=Mod, ref=Ref, model=Model};
-	{error, _} = Err ->
-	    Err
+		{ok, Ref} ->
+			#state{mod=Mod, ref=Ref, model=Model};
+		{error, _} = Err ->
+			Err
     end.
 
 -spec run(Config :: econfig_config:t(), Frontend :: t()) -> {ok, econfig_config:t(), t()} | {error, econfig_err()}.
 run(Config, #state{model=Model, mod=Mod, ref=Ref}=F) ->
     try Mod:run(Model, Config, Ref) of
-	{ok, C1, Ref1} ->
-	    {ok, C1, F#state{ref=Ref1}};
-	{error, _} = Err ->
-	    Mod:format_error(Err),
-	    Err
+		{ok, C1, Ref1} ->
+			{ok, C1, F#state{ref=Ref1}};
+		{error, _} = Err ->
+			Mod:format_error(Err),
+			Err
     catch
-	throw:Err ->
-	    Mod:format_error(Err),
-	    Err
+		throw:Err ->
+			Mod:format_error(Err),
+			Err
     end.
 
 %%%
@@ -59,16 +59,16 @@ run(Config, #state{model=Model, mod=Mod, ref=Ref}=F) ->
 %%%
 impl() ->
     case application:get_env(econfig, frontend, tty) of
-	I when is_atom(I) ->
-	    Mod = list_to_atom("econfig_ui_" ++ atom_to_list(I)),
-	    case is_module(Mod) of
-		true ->
-		    Mod;
-		false ->
-		    throw({invalid_frontend, Mod})
-	    end;
-	Else ->
-	    throw({invalid_frontend, Else})
+		I when is_atom(I) ->
+			Mod = list_to_atom("econfig_ui_" ++ atom_to_list(I)),
+			case is_module(Mod) of
+				true ->
+					Mod;
+				false ->
+					throw({invalid_frontend, Mod})
+			end;
+		Else ->
+			throw({invalid_frontend, Else})
     end.
 
 is_module(Mod) when is_atom(Mod) ->
