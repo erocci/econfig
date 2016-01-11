78b1e44810631a03f8a4899a11dda77229462012
Jean Parpaillon <jean.parpaillon@free.fr>
2016-01-11 16:29:31 +0100

    Fix indent

--- include/econfig.hrl
+++ include/econfig.hrl
@@ -4,40 +4,40 @@
 -define(econfig_file, ".econfig").
 
 -type econfig_entry() :: {Key     :: econfig_entry_key(),
-			  Desc    :: string(), 
-			  Type    :: econfig_entry_type(),
-			  Default :: term(),
-			  Options :: [econfig_entry_opt()]}.
+						  Desc    :: string(), 
+						  Type    :: econfig_entry_type(),
+						  Default :: term(),
+						  Options :: [econfig_entry_opt()]}.
 
 -type econfig_entry_type() :: boolean
-			    | string
-			    | {enum, [atom()]}
-			    | integer 
-			    | {range, Low :: integer(), High :: integer()}.
+							| string
+							| {enum, [atom()]}
+							| integer 
+							| {range, Low :: integer(), High :: integer()}.
 
 -type econfig_entry_opt() :: {select,    [econfig_entry_key()]}
-			   | {depends,   [econfig_entry_dep()]}
-			   | {menu,      [econfig_entry_dep()]}
-			   | {choice,    [econfig_entry_dep()]}
-			   | {requires,  [econfig_entry_dep()]}
-			   | {excludes,  [econfig_entry_dep()]}
-			   | {help,      string()}
-			   | {priority,  integer()}
-			   | {call,      mfa()}.
+						   | {depends,   [econfig_entry_dep()]}
+						   | {menu,      [econfig_entry_dep()]}
+						   | {choice,    [econfig_entry_dep()]}
+						   | {requires,  [econfig_entry_dep()]}
+						   | {excludes,  [econfig_entry_dep()]}
+						   | {help,      string()}
+						   | {priority,  integer()}
+						   | {call,      mfa()}.
 
 -type econfig_entry_dep() :: {econfig_entry_key(), econfig_entry_def()}
-			   | econfig_entry_key().
+						   | econfig_entry_key().
 
 -type econfig_entry_key() :: atom().
 
 -type econfig_entry_def() :: boolean()
-			   | integer()
-			   | string() 
-			   | atom()
-			   | {choice, list()}
-			   | {lt, integer()}
-			   | {gt, integer()}
-			   | '_'.
+						   | integer()
+						   | string() 
+						   | atom()
+						   | {choice, list()}
+						   | {lt, integer()}
+						   | {gt, integer()}
+						   | '_'.
 
 -type econfig_config() :: [econfig_value_entry()].
 
